efabc3a16a149b1392c5cd67771580cd6319ba1a
周洲游 <1445352275@qq.com>
2019-12-10 23:06:38 +0800

    删除了冗余的js和css

--- resource/template/js/common.js
+++ resource/template/js/common.js
@@ -3,8 +3,31 @@ function successLogin(result) {
     window.location.replace("welcome");
 }
 
+function getToken() {
+    return Cookies.get('token');
+}
+
 function isValid(control) {
     return control.hasClass("is-valid");
+}
+
+function addTd(object, tr) {
+    let keys = Object.keys(object);
+    for (let i = 0; i < keys.length; i++) {
+        let value = object[keys[i]];
+        let td = $("<td></td>");
+        td.text(value);
+        tr.append(td);
+    }
+}
+
+function addTr(collection, tableBody, additionalTr) {
+    for (let i = 0; i < collection.length; i++) {
+        let tr = $("<tr></tr>");
+        addTd(collection[i], tr);
+        additionalTr(collection[i], tr);
+        tableBody.append(tr);
+    }
 }
 
 function sendRequest(method, uri, data, successFunc, failFunc) {
